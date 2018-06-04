cea2c9e129625511022fd33ca89a34e853550ebb
Troy <36167978+troywsmith@users.noreply.github.com>
2018-06-04 10:59:36 -0400

    changed connect file

--- README.md
+++ README.md
@@ -1,19 +1,2 @@
 # project-two
 Crypto Bank Application: Swap
-
-possible names:
-swap
-shift
-chop chop
-pronto
-jiffy
-flash
-
-# SWAP tokens
-The Token’s Name: Swap Token
-The Token’s Symbol: SWAP
-The Token’s Decimal Places: 0
-The Number of Tokens in Circulation: 100,000,000
-
-Solidity Version:
-0.4.24+commit.e67f0147
--- database/connection.js
+++ database/connection.js
@@ -3,6 +3,7 @@ const monitor = require("pg-monitor");
 
 let initOptions = {};
 
+// Display better error stack traces in development.
 if (process.NODE_ENV !== "production") {
   promise.config({
     longStackTraces: true
@@ -12,12 +13,25 @@ if (process.NODE_ENV !== "production") {
   };
 }
 
+// attach to all events at once;
 monitor.attach(initOptions, ["query", "error"]);
 
+// Import pg-promise and initialize the library with an empty object.
 const pgp = require("pg-promise")(initOptions);
 
-const connectionURL = "postgres://localhost:5432/cryptobank_db";
+const databaseName = "cryptobank_app";
 
-const db = pgp(connectionURL);
+let connectionConfig;
+
+if (process.env.NODE_ENV === "development" || !process.env.NODE_ENV) {
+  // Prepare the connection URL from the format: 'postgres://username:password@host:port/database';
+  connectionConfig = `postgres://localhost:5432/${databaseName}`;
+} else if (process.env.NODE_ENV === "production") {
+  // Heroku will set this variable for you.
+  connectionConfig = process.env.DATABASE_URL;
+}
+
+// Creating a new database connection with the provided configuration.
+const db = pgp(connectionConfig);
 
 module.exports = db;
