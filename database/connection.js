96a25b315ce5442f54561a22e2b6d365931f07aa
Troy <36167978+troywsmith@users.noreply.github.com>
2018-06-04 11:03:39 -0400

    changed db connection

--- database/connection.js
+++ database/connection.js
@@ -19,7 +19,7 @@ monitor.attach(initOptions, ["query", "error"]);
 // Import pg-promise and initialize the library with an empty object.
 const pgp = require("pg-promise")(initOptions);
 
-const databaseName = "cryptobank_app";
+const databaseName = "cryptobank_db";
 
 let connectionConfig;
 
